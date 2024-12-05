4b12dc819a5fd689523180795e803440bd4fe968
JJH <whwndgnl12@naver.com>
2024-12-05 12:22:30 +0900

    feat:stadiumService 테스트코드 작성(구장 정보 수정, 구장 존재하지 않음)

--- src/test/java/com/sparta/sportify/service/stadium/StadiumServiceTest.java
+++ src/test/java/com/sparta/sportify/service/stadium/StadiumServiceTest.java
@@ -13,6 +13,7 @@ import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
 import com.sparta.sportify.dto.stadium.request.StadiumCreateRequestDto;
+import com.sparta.sportify.dto.stadium.request.StadiumUpdateRequestDto;
 import com.sparta.sportify.dto.stadium.response.StadiumResponseDto;
 import com.sparta.sportify.entity.Stadium;
 import com.sparta.sportify.repository.StadiumRepository;
@@ -26,11 +27,16 @@ class StadiumServiceTest {
 	private StadiumService stadiumService;
 
 	private StadiumCreateRequestDto stadiumCreateRequestDto;
+	private StadiumUpdateRequestDto stadiumUpdateRequestDto;
 
+	private Long stadiumId;
 	@BeforeEach
 	void setUp() {
 		MockitoAnnotations.openMocks(this);
+
+		stadiumId = 1L;
 		stadiumCreateRequestDto = new StadiumCreateRequestDto("A구장", "서울", 6, 6, "넓고 좋은 경기장", 100000);
+		stadiumUpdateRequestDto = new StadiumUpdateRequestDto("B구장", "서울", 6, 6, "넓고 좋은 경기장", 100000);
 	}
 
 	@Test
@@ -42,7 +48,7 @@ class StadiumServiceTest {
 			.thenReturn(Optional.empty());
 		when(stadiumRepository.save(any(Stadium.class))).thenReturn(stadium);
 
-		assertNotNull(stadiumService.CreateStadium(stadiumCreateRequestDto));
+		assertNotNull(stadiumService.createStadium(stadiumCreateRequestDto));
 		verify(stadiumRepository, times(1)).save(any(Stadium.class));
 	}
 
@@ -57,10 +63,39 @@ class StadiumServiceTest {
 		when(stadiumRepository.save(any(Stadium.class))).thenReturn(stadium);
 
 		IllegalArgumentException thrown = assertThrows(IllegalArgumentException.class, () -> {
-			stadiumService.CreateStadium(stadiumCreateRequestDto);
+			stadiumService.createStadium(stadiumCreateRequestDto);
 		});
 
 		assertEquals("구장 이름이 이미 존재합니다", thrown.getMessage());
+		verify(stadiumRepository, times(0)).save(any(Stadium.class));
+	}
+
+	@Test
+	@DisplayName("구장 정보 수정")
+	void updateStadium() {
+		Stadium stadium = Stadium.createOf(stadiumCreateRequestDto);
+		when(stadiumRepository.findById(stadiumId)).thenReturn(Optional.of(stadium));
+		when(stadiumRepository.save(any(Stadium.class))).thenReturn(stadium);
+
+		stadiumService.updateStadium(stadiumId, stadiumUpdateRequestDto);
+
+		assertEquals(stadiumUpdateRequestDto.getStadiumName(), stadium.getStadiumName());
+		assertEquals(stadiumUpdateRequestDto.getLocation(), stadium.getLocation());
+		assertEquals(stadiumUpdateRequestDto.getATeamCount(), stadium.getATeamCount());
+		assertEquals(stadiumUpdateRequestDto.getBTeamCount(), stadium.getBTeamCount());
+		assertEquals(stadiumUpdateRequestDto.getDescription(), stadium.getDescription());
+		assertEquals(stadiumUpdateRequestDto.getPrice(), stadium.getPrice());
+	}
+
+	@Test
+	@DisplayName("구장 정보 수정")
+	void notFoundStadium() {
+		when(stadiumRepository.findById(stadiumId)).thenReturn(Optional.empty());
+		IllegalArgumentException thrown = assertThrows(IllegalArgumentException.class, () -> {
+			stadiumService.updateStadium(stadiumId, stadiumUpdateRequestDto);
+		});
+
+		assertEquals("구장이 존재하지 않습니다", thrown.getMessage());
 		verify(stadiumRepository, times(0)).save(any(Stadium.class));
 	}
 }
