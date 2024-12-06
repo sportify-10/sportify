2fd1cfdec31c5d1d7794beda282ded2754bdc25b
JJH <whwndgnl12@naver.com>
2024-12-06 14:40:01 +0900

    feat:자신의 구장 조회 구현

--- src/main/java/com/sparta/sportify/controller/stadium/StadiumController.java
+++ src/main/java/com/sparta/sportify/controller/stadium/StadiumController.java
@@ -1,13 +1,17 @@
 package com.sparta.sportify.controller.stadium;
 
+import java.util.List;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.sparta.sportify.dto.stadium.request.StadiumCreateRequestDto;
@@ -33,6 +37,15 @@ public class StadiumController {
 	) {
 
 		return ResponseEntity.ok(ApiResult.success(stadiumService.createStadium(stadiumCreateRequestDto, userDetails)));
+	}
+
+	@GetMapping
+	public ResponseEntity<ApiResult<List<StadiumResponseDto>>> getStadiums(
+		@AuthenticationPrincipal UserDetailsImpl userDetails,
+		@RequestParam(defaultValue = "1") int page,
+		@RequestParam(defaultValue = "5") int size
+	) {
+		return ResponseEntity.ok(ApiResult.success(stadiumService.getStadiums(userDetails, page, size)));
 	}
 
 	@PatchMapping("/{stadiumId}")
