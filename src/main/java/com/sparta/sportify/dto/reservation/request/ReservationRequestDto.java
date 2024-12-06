f4b0fcbfff38fb8e1e092549ad38b01296377ed6
caose <rlagkrwls258@gmail.com>
2024-12-06 13:09:05 +0900

    feat : DTO notBlank로 변경

--- src/main/java/com/sparta/sportify/dto/reservation/request/ReservationRequestDto.java
+++ src/main/java/com/sparta/sportify/dto/reservation/request/ReservationRequestDto.java
@@ -1,9 +1,7 @@
 package com.sparta.sportify.dto.reservation.request;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NonNull;
-import lombok.Setter;
+import jakarta.validation.constraints.NotBlank;
+import lombok.*;
 
 import java.time.LocalDate;
 import java.util.List;
@@ -11,23 +9,24 @@ import java.util.List;
 @Getter
 @Setter
 @AllArgsConstructor
+@NoArgsConstructor
 public class ReservationRequestDto {
 
 
     long teamId;
 
-    @NonNull
+    @NotBlank(message = "구장에 대한 시간 선택은 필수 항목입니다..")
     Long stadiumTimeId;
 
-    @NonNull
+    @NotBlank(message = "날짜 데이터는 필수 항목입니다.")
     LocalDate reservationDate;
 
-    @NonNull
+    @NotBlank(message = "팀 선택은 필수 항목입니다. (A/B).")
     char teamColor;
 
     List<Integer> teamMemberIdList;
 
-    @NonNull
+    @NotBlank(message = "시간 선택은 필수 사항입니다..")
     int time;
 
 }
