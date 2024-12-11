6bfeb42970264550f630345b5bee1de115a23dd2
JJH <whwndgnl12@naver.com>
2024-12-11 15:38:46 +0900

    feat:캐시 사용 내역 조회

--- src/main/java/com/sparta/sportify/controller/CashController.java
+++ src/main/java/com/sparta/sportify/controller/CashController.java
@@ -1,13 +1,18 @@
 package com.sparta.sportify.controller;
 
+import java.util.List;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.sparta.sportify.dto.cash.request.CashRequestDto;
+import com.sparta.sportify.dto.cash.response.CashLogsResponseDto;
 import com.sparta.sportify.dto.cash.response.CashResponseDto;
 import com.sparta.sportify.security.UserDetailsImpl;
 import com.sparta.sportify.service.CashService;
@@ -27,5 +32,14 @@ public class CashController {
 		@RequestBody CashRequestDto cashRequestDto
 	){
 		return ResponseEntity.ok(ApiResult.success("캐시 충전 성공", cashService.addCash(userDetails, cashRequestDto)));
+	}
+
+	@GetMapping
+	public ResponseEntity<ApiResult<List<CashLogsResponseDto>>> getCashLogs(
+		@AuthenticationPrincipal UserDetailsImpl userDetails,
+		@RequestParam(defaultValue = "1") int page,
+		@RequestParam(defaultValue = "5") int size
+	) {
+		return ResponseEntity.ok(ApiResult.success("캐시 로그 조회 성공",cashService.getCashLogs(userDetails, page, size)));
 	}
 }
--- src/main/java/com/sparta/sportify/service/CashService.java
+++ src/main/java/com/sparta/sportify/service/CashService.java
@@ -1,10 +1,16 @@
 package com.sparta.sportify.service;
 
 import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import com.sparta.sportify.dto.cash.request.CashRequestDto;
+import com.sparta.sportify.dto.cash.response.CashLogsResponseDto;
 import com.sparta.sportify.dto.cash.response.CashResponseDto;
 import com.sparta.sportify.entity.CashLog;
 import com.sparta.sportify.entity.CashType;
@@ -35,5 +41,20 @@ public class CashService {
 		userRepository.save(userDetails.getUser());
 
 		return new CashResponseDto(cashLog);
+	}
+
+	public List<CashLogsResponseDto> getCashLogs(UserDetailsImpl userDetails, int page, int size) {
+		Pageable pageable = PageRequest.of(page-1, size);
+		Page<CashLog> cashLogs = cashLogRepository.findAllByUserId(userDetails.getUser().getId(), pageable);
+
+		if(cashLogs.isEmpty()) {
+			throw new IllegalArgumentException("캐시 사용 내역이 없습니다");
+		}
+
+		return cashLogs.stream().map(cashLog -> new CashLogsResponseDto(
+			cashLog.getPrice(),
+			cashLog.getCreateAt(),
+			cashLog.getType()
+		)).collect(Collectors.toList());
 	}
 }
