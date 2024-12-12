f0d941511be91b1da6cebe71b93de0d439f8f5fb
yeonwoochocho <cyw614c@gmail.com>
2024-12-12 15:14:57 +0900

    refactor : restTemplate 제거

--- src/main/java/com/sparta/sportify/jwt/JwtAuthenticationFilter.java
+++ src/main/java/com/sparta/sportify/jwt/JwtAuthenticationFilter.java
@@ -1,18 +1,12 @@
 package com.sparta.sportify.jwt;
 
-import com.sparta.sportify.dto.user.req.OAuthUserInfo;
-import com.sparta.sportify.dto.user.res.KakaoUserResponse;
-import com.sparta.sportify.exception.CustomValidationException;
 import com.sparta.sportify.security.UserDetailsImpl;
-import io.jsonwebtoken.Jwt;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.*;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -22,9 +16,6 @@ import org.springframework.web.client.RestTemplate;
 import org.springframework.web.filter.OncePerRequestFilter;
 import java.io.IOException;
 import java.rmi.RemoteException;
-import com.sparta.sportify.config.RestTemplateConfig;
-
-import com.sparta.sportify.jwt.JwtUtil;
 
 @Slf4j
 @Component
@@ -32,12 +23,12 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
     private final JwtUtil jwtUtil;
     private final UserDetailsService userDetailsService;
-    private final RestTemplate restTemplate;
 
-    public JwtAuthenticationFilter(JwtUtil jwtUtil, UserDetailsService userDetailsService, RestTemplate restTemplate) {
+
+    public JwtAuthenticationFilter(JwtUtil jwtUtil, UserDetailsService userDetailsService) {
         this.jwtUtil = jwtUtil;
         this.userDetailsService = userDetailsService;
-        this.restTemplate = restTemplate;
+
     }
 
     @Override
