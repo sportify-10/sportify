04f18662e61890155f81859ca9d2a276f08076a2
정현우 <jhw8449@gmail.com>
2024-12-17 20:49:23 +0900

    feat: cache 설정 및 조회시 성능 개선

--- src/main/java/com/sparta/sportify/config/RedissonConfig.java
+++ src/main/java/com/sparta/sportify/config/RedissonConfig.java
@@ -14,13 +14,15 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
 import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.RedisSerializationContext;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 
 @EnableCaching
 @Configuration
@@ -44,7 +46,15 @@ public class RedissonConfig {
 
 	@Bean
 	public ObjectMapper objectMapper() {
-		return new ObjectMapper();
+		ObjectMapper objectMapper = new ObjectMapper();
+		objectMapper.registerModule(new JavaTimeModule());
+		objectMapper.activateDefaultTyping(
+			objectMapper.getPolymorphicTypeValidator(),
+			ObjectMapper.DefaultTyping.EVERYTHING,
+			JsonTypeInfo.As.WRAPPER_OBJECT
+		);
+		objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+		return objectMapper;
 	}
 
 	// spring의 캐시 타입 Redis로 설정한다. (이 과정이 있다면 spring.cache.type 설정 생략 가능)
@@ -56,24 +66,23 @@ public class RedissonConfig {
 			.build();
 	}
 
-	// Redis 연결 설정
+	// Redis 연결 설정 (Redisson)
 	@Bean
 	public RedisConnectionFactory redisConnectionFactory() {
 		return new RedissonConnectionFactory();
 	}
 
-	// Redis 캐시 설정
+	// Redis 캐시 설정 (캐시된 데이터가 1일 후에 만료)
 	@Bean
 	public RedisCacheConfiguration redisCacheConfiguration() {
 		return RedisCacheConfiguration.defaultCacheConfig()
 			.serializeKeysWith(RedisSerializationContext.SerializationPair.fromSerializer(new StringRedisSerializer())) // 키를 String으로 직렬화해 저장
 			.serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(new GenericJackson2JsonRedisSerializer(objectMapper()))) // 객체를 JSON으로 바꿔서 Redis에 저장
-			.entryTtl(Duration.ofDays(1L));
+			.entryTtl(Duration.ofDays(1L)); //TTL을 1일로 설정
 	}
 
-	// Redis의 세밀한 조작이 필요할 경우 RedisTemplate를 빈 등록하면된다.
+	// Redis의 세밀한 조작이 필요할 경우 RedisTemplate를 빈 등록
 	// Redis의 모든 자료구조 다 쓸 수 있음 (String, List, Hash, Set 등)
-	// 간단한 캐시 조작이라면 추가하지 않아도 된다.
 	// @Bean
 	// public RedisTemplate<?, ?> redisTemplate() {
 	// 	RedisTemplate<?, ?> redisTemplate = new RedisTemplate<>();
--- src/main/java/com/sparta/sportify/dto/user/res/SignupResponseDto.java
+++ src/main/java/com/sparta/sportify/dto/user/res/SignupResponseDto.java
@@ -1,11 +1,9 @@
 package com.sparta.sportify.dto.user.res;
-
 import com.sparta.sportify.entity.User;
 import com.sparta.sportify.entity.UserRole;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
-
 @Getter
 @Setter
 @AllArgsConstructor
@@ -16,7 +14,10 @@ public class SignupResponseDto {
     private String region;
     private Long age;
     private String gender;
+    private Long levelPoints;  // 새로운 필드 추가
+    // private Long userRanking;
 
+    // User와 JWT 토큰으로부터 DTO를 생성하는 생성자
     public SignupResponseDto(User user, String jwtToken) {
         this.email = user.getEmail();
         this.name = user.getName();
@@ -24,9 +25,14 @@ public class SignupResponseDto {
         this.region = user.getRegion();  // 지역 정보
         this.age = user.getAge();  // 나이
         this.gender = user.getGender();  // 성별
+        this.levelPoints = user.getLevelPoints();  // levelPoints 추가
+        // this.userRanking = user.getUserRanking();
     }
-
-
+    // 테스트용 생성자
     public SignupResponseDto(long l, String testUsername, String mail) {
+        this.email = mail;
+        this.name = testUsername;
+        this.levelPoints = l;  // levelPoints 추가
+        // this.userRanking = r;
     }
 }
