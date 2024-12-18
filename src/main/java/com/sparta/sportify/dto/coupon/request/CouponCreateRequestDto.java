d62462a646d5549cd0a95ef83f273f1da4cc304c
caose <rlagkrwls258@gmail.com>
2024-12-18 09:40:55 +0900

    feat : 누락된 내용 수정

--- src/main/java/com/sparta/sportify/dto/coupon/request/CouponCreateRequestDto.java
+++ src/main/java/com/sparta/sportify/dto/coupon/request/CouponCreateRequestDto.java
@@ -1,10 +1,14 @@
 package com.sparta.sportify.dto.coupon.request;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
 
 @Getter
+@AllArgsConstructor
+@NoArgsConstructor
 public class CouponCreateRequestDto {
     String code;
 
