5d43bc1a9bd5d76a996dd6382c86ef49327600a7
caose <rlagkrwls258@gmail.com>
2024-12-18 09:53:56 +0900

    feat : 쿠폰 중복사용 방지 및 Create_at활성

--- src/main/java/com/sparta/sportify/entity/Coupon.java
+++ src/main/java/com/sparta/sportify/entity/Coupon.java
@@ -6,11 +6,13 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 @Entity
+@EntityListeners(AuditingEntityListener.class)
 @Table(name = "coupons")
 @AllArgsConstructor
 @NoArgsConstructor
