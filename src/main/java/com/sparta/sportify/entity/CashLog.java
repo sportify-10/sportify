a802082f9408f3d9d5e7c92a4891837c6e647397
caose <rlagkrwls258@gmail.com>
2024-12-18 09:53:56 +0900

    feat : 쿠폰 중복사용 방지 및 Create_at활성

--- src/main/java/com/sparta/sportify/entity/CashLog.java
+++ src/main/java/com/sparta/sportify/entity/CashLog.java
@@ -6,10 +6,12 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 
 @Entity
+@EntityListeners(AuditingEntityListener.class)
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
