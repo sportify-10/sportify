80075a0b5384d6a2b7aa4f5a124f6786821967f3
JJH <whwndgnl12@naver.com>
2024-12-18 16:54:50 +0900

    feat:팀 게시판-게시글 삭제

--- src/main/java/com/sparta/sportify/controller/teamArticle/TeamArticleController.java
+++ src/main/java/com/sparta/sportify/controller/teamArticle/TeamArticleController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -55,5 +56,13 @@ public class TeamArticleController {
 		@AuthenticationPrincipal UserDetailsImpl userDetails
 	){
 		return ResponseEntity.ok(ApiResult.success("게시글 수정 성공",teamArticleService.updatePost(articleId, teamArticleRequestDto, userDetails)));
+	}
+
+	@DeleteMapping("/{articleId}")
+	public ResponseEntity<ApiResult<TeamArticleResponseDto>> deletePost(
+		@PathVariable Long articleId,
+		@AuthenticationPrincipal UserDetailsImpl userDetails
+	){
+		return ResponseEntity.ok(ApiResult.success("게시글 삭제 성공", teamArticleService.deletePost(articleId, userDetails)));
 	}
 }
