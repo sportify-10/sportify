df7113d39817e71996196455b0783e10a5d8385a
caose <rlagkrwls258@gmail.com>
2024-12-26 11:48:59 +0900

    refactor : 전체 리팩토링 수정

--- src/main/java/com/sparta/sportify/entity/match/Match.java
+++ src/main/java/com/sparta/sportify/entity/match/Match.java
@@ -20,8 +20,6 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "matchs")
