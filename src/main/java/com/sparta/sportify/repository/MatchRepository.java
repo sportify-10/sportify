5244ca30e5118add22896bef37bb216f8483d715
yeonwoochocho <cyw614c@gmail.com>
2024-12-26 15:25:08 +0900

    feat : 경기 1시간 전 경기 알림 기능 구현

--- src/main/java/com/sparta/sportify/repository/MatchRepository.java
+++ src/main/java/com/sparta/sportify/repository/MatchRepository.java
@@ -2,9 +2,18 @@ package com.sparta.sportify.repository;
 
 import com.sparta.sportify.entity.match.Match;
 import com.sparta.sportify.entity.reservation.ReservationStatus;
+import java.time.LocalDate;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
+import io.lettuce.core.dynamic.annotation.Param;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.sparta.sportify.entity.match.Match;
 import org.springframework.data.jpa.repository.Query;
 
 import java.time.LocalDate;
@@ -12,7 +21,6 @@ import java.util.Optional;
 
 public interface MatchRepository extends JpaRepository<Match, Long> {
 	Optional<Match> findByIdAndDateAndTime(Long id, LocalDate date, Integer time);
-
 	Optional<Match> findByStadiumTimeIdAndDateAndTime(Long id, LocalDate date, Integer time);
 
 	@Query("SELECT m, SUM(r.totalAmount) " +
@@ -24,4 +32,6 @@ public interface MatchRepository extends JpaRepository<Match, Long> {
 		"GROUP BY m.id")
 	Page<Object[]> findMatchesWithTotalAmountByStadiumId(Long stadiumId, ReservationStatus status, Pageable pageable);
 }
+
+
 
