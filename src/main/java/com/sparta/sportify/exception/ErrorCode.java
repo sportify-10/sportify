2cc10dfe7626a1936f92f4ec532e2ad92a937bed
정현우 <jhw8449@gmail.com>
2024-12-26 19:40:22 +0900

    refactor: 캐시서비스 에러코드 수정

--- src/main/java/com/sparta/sportify/exception/ErrorCode.java
+++ src/main/java/com/sparta/sportify/exception/ErrorCode.java
@@ -1,20 +1,21 @@
 package com.sparta.sportify.exception;
 
 import lombok.Getter;
+
 import org.springframework.http.HttpStatus;
 
 @Getter
 public enum ErrorCode {
 
-    USER_NOT_FOUND(404, "존재하지 않는 유저입니다");
+	USER_NOT_FOUND(404, "존재하지 않는 유저입니다"),
+	CASH_LOG_NOT_FOUND(404, "캐시 사용 내역이 없습니다.");
 
-    private final int status;
-    private final String message;
+	private final int status;
+	private final String message;
 
-    ErrorCode(int status, String message) {
-        this.status = status;
-        this.message = message;
-    }
-
+	ErrorCode(int status, String message) {
+		this.status = status;
+		this.message = message;
+	}
 
 }
--- src/main/java/com/sparta/sportify/service/CashService.java
+++ src/main/java/com/sparta/sportify/service/CashService.java
@@ -7,10 +7,14 @@ import com.sparta.sportify.dto.kakaoPay.request.KakaoPayApproveRequestDto;
 import com.sparta.sportify.dto.kakaoPay.response.KakaoPayReadyResponseDto;
 import com.sparta.sportify.entity.cashLog.CashLog;
 import com.sparta.sportify.entity.cashLog.CashType;
+import com.sparta.sportify.exception.CustomApiException;
+import com.sparta.sportify.exception.ErrorCode;
 import com.sparta.sportify.repository.CashLogRepository;
 import com.sparta.sportify.repository.UserRepository;
 import com.sparta.sportify.security.UserDetailsImpl;
+
 import lombok.RequiredArgsConstructor;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
@@ -26,7 +30,6 @@ public class CashService {
 	private final UserRepository userRepository;
 	private final KakaoPayService kakaoPayService;
 
-
 	/**
 	 * 결제 준비 요청 (결제 URL 반환)
 	 */
@@ -38,12 +41,12 @@ public class CashService {
 		kakaoPayService.approvePayment(request);
 
 		CashLog cashLog = cashLogRepository.save(
-				CashLog.builder()
-						.price(request.getAmount())
-						.createAt(LocalDateTime.now())
-						.type(CashType.CHARGE)
-						.user(userDetails.getUser())
-						.build());
+			CashLog.builder()
+				.price(request.getAmount())
+				.createAt(LocalDateTime.now())
+				.type(CashType.CHARGE)
+				.user(userDetails.getUser())
+				.build());
 
 		userDetails.getUser().addCash(request.getAmount());
 		userRepository.save(userDetails.getUser());
@@ -54,12 +57,12 @@ public class CashService {
 	public CashResponseDto CashRefund(UserDetailsImpl userDetails, CashRequestDto request, String tid) {
 		kakaoPayService.refundPayment(request, tid);
 		CashLog cashLog = cashLogRepository.save(
-				CashLog.builder()
-						.price(-(request.getAmount()))
-						.createAt(LocalDateTime.now())
-						.type(CashType.REFUND)
-						.user(userDetails.getUser())
-						.build());
+			CashLog.builder()
+				.price(-(request.getAmount()))
+				.createAt(LocalDateTime.now())
+				.type(CashType.REFUND)
+				.user(userDetails.getUser())
+				.build());
 		userDetails.getUser().subCash(request.getAmount());
 		userRepository.save(userDetails.getUser());
 
@@ -67,11 +70,11 @@ public class CashService {
 	}
 
 	public Page<CashLogsResponseDto> getCashLogs(UserDetailsImpl userDetails, int page, int size) {
-		Pageable pageable = PageRequest.of(page-1, size);
+		Pageable pageable = PageRequest.of(page - 1, size);
 		Page<CashLog> cashLogs = cashLogRepository.findAllByUserId(userDetails.getUser().getId(), pageable);
 
-		if(cashLogs.isEmpty()) {
-			throw new IllegalArgumentException("캐시 사용 내역이 없습니다");
+		if (cashLogs.isEmpty()) {
+			throw new CustomApiException(ErrorCode.CASH_LOG_NOT_FOUND);
 		}
 
 		return cashLogs.map(cashLog -> new CashLogsResponseDto(
