deca718979e5e930aeead32979e7b65d18ebc398
caose <rlagkrwls258@gmail.com>
2024-12-26 15:46:26 +0900

    refactor : 쿠폰 Validation 추가

--- src/main/java/com/sparta/sportify/dto/coupon/request/CouponUseRequestDto.java
+++ src/main/java/com/sparta/sportify/dto/coupon/request/CouponUseRequestDto.java
@@ -1,11 +1,12 @@
 package com.sparta.sportify.dto.coupon.request;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 public class CouponUseRequestDto {
+    @NotBlank
     String code;
-
 }
