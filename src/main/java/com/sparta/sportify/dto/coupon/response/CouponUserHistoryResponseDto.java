c495d8c3c01b25a75bc77c3bda87639e5449d51c
caose <rlagkrwls258@gmail.com>
2024-12-30 13:56:29 +0900

    feat : 쿠폰 dto Getter추가

--- src/main/java/com/sparta/sportify/dto/coupon/response/CouponUserHistoryResponseDto.java
+++ src/main/java/com/sparta/sportify/dto/coupon/response/CouponUserHistoryResponseDto.java
@@ -1,10 +1,12 @@
 package com.sparta.sportify.dto.coupon.response;
 
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 
 import java.time.LocalDateTime;
 
 @AllArgsConstructor
+@Getter
 public class CouponUserHistoryResponseDto {
     private String name;
     private String code;
