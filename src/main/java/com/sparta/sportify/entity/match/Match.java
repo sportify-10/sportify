10f9f9657950b77f728e4933d2e4a63f97e40450
yeonwoochocho <cyw614c@gmail.com>
2024-12-30 10:02:04 +0900

    refactor : consumer 메시지 출력 수정

--- src/main/java/com/sparta/sportify/entity/match/Match.java
+++ src/main/java/com/sparta/sportify/entity/match/Match.java
@@ -22,7 +22,7 @@ import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 
 @Entity
-@Table(name = "matches")
+@Table(name = "matchs")
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
