0cf159d3ed786891976329244da1a4aa23eb87a5
music <musicmalgon@gmail.com>
2024-12-31 23:37:04 +0900

    feat: 캐쉬 컨트롤러 테스트코드 완료

--- src/main/java/com/sparta/sportify/dto/cash/response/CashResponseDto.java
+++ src/main/java/com/sparta/sportify/dto/cash/response/CashResponseDto.java
@@ -5,11 +5,13 @@ import com.sparta.sportify.entity.cashLog.CashType;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.time.LocalDateTime;
 
 @Getter
 @AllArgsConstructor
+@Setter
 @NoArgsConstructor
 public class CashResponseDto {
     private Long userId;
