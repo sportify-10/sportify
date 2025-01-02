05f373f9cd11081fe4d0afbeae665998df50ffe8
caose <rlagkrwls258@gmail.com>
2025-01-02 17:26:15 +0900

    fix : dto 생성자 중복 에러 제거

--- src/main/java/com/sparta/sportify/dto/match/response/MatchByStadiumResponseDto.java
+++ src/main/java/com/sparta/sportify/dto/match/response/MatchByStadiumResponseDto.java
@@ -3,13 +3,11 @@ package com.sparta.sportify.dto.match.response;
 import com.sparta.sportify.entity.matchResult.MatchStatus;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 
 @Getter
 @RequiredArgsConstructor
 @AllArgsConstructor
-@NoArgsConstructor
 public class MatchByStadiumResponseDto {
     public Long stadiumId;
     public String stadiumName;
