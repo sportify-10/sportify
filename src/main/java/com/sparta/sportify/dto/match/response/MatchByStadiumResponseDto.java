d63ebdac36de87042b43b49140d713bb470c41e5
caose <rlagkrwls258@gmail.com>
2025-01-02 17:24:50 +0900

    fix : dto final 삭제

--- src/main/java/com/sparta/sportify/dto/match/response/MatchByStadiumResponseDto.java
+++ src/main/java/com/sparta/sportify/dto/match/response/MatchByStadiumResponseDto.java
@@ -1,18 +1,21 @@
 package com.sparta.sportify.dto.match.response;
 
 import com.sparta.sportify.entity.matchResult.MatchStatus;
-
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 
 @Getter
 @RequiredArgsConstructor
+@AllArgsConstructor
+@NoArgsConstructor
 public class MatchByStadiumResponseDto {
-	private final Long stadiumId;
-	private final String stadiumName;
-	private final String stadiumDescription;
-	private final String stadiumLocation;
-	private final String startTime;
-	private final String endTime;
-	private final MatchStatus status;
+    public Long stadiumId;
+    public String stadiumName;
+    public String stadiumDescription;
+    public String stadiumLocation;
+    public String startTime;
+    public String endTime;
+    public MatchStatus status;
 }
