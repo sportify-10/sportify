418bcfa6973172ccf10687b716d8cb0240de7dcc
caose <rlagkrwls258@gmail.com>
2025-01-06 11:47:26 +0900

    fix :에러수정

--- src/main/java/com/sparta/sportify/config/SecurityConfig.java
+++ src/main/java/com/sparta/sportify/config/SecurityConfig.java
@@ -1,10 +1,11 @@
 package com.sparta.sportify.config;
 
-import com.sparta.sportify.exception.CustomApiException;
-import com.sparta.sportify.exception.ErrorCode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.sparta.sportify.jwt.JwtAuthenticationFilter;
 import com.sparta.sportify.security.OAuth2LoginSuccessHandler;
 import com.sparta.sportify.service.oauth.CustomOAuth2UserService;
+import com.sparta.sportify.util.api.ApiResult;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -25,7 +26,6 @@ public class SecurityConfig {
     private final CustomOAuth2UserService customOAuth2UserService;
 
     private final OAuth2LoginSuccessHandler successHandler;
-
 
     public SecurityConfig(@Qualifier("customPasswordEncoder") CustomPasswordEncoder customPasswordEncoder, JwtAuthenticationFilter jwtAuthenticationFilter, CustomOAuth2UserService customOAuth2UserService, OAuth2LoginSuccessHandler successHandler) {
         this.customPasswordEncoder = customPasswordEncoder;
@@ -64,9 +64,31 @@ public class SecurityConfig {
                         .successHandler(successHandler) // 성공 핸들러 등록
                 )
                 .exceptionHandling(exception -> exception
-                        .authenticationEntryPoint((request, response, authException) -> {
-                            throw new CustomApiException(ErrorCode.UNAUTHORIZED);
-                        })
+                                .authenticationEntryPoint((request, response, authException) -> {
+//                            response.sendError(HttpServletResponse.SC_UNAUTHORIZED);
+                                    response.setContentType("application/json");
+                                    response.setCharacterEncoding("UTF-8");
+                                    response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+
+                                    // ApiResult 형식으로 에러 메시지 생성
+//                                    ApiResult<?> errorResult = ApiResult.error(401, "인증x");
+                                    ApiResult<?> errorResult = ApiResult.error(401, "인증x", 401);
+
+
+                                    // ObjectMapper로 객체를 JSON으로 변환하여 응답에 작성
+                                    ObjectMapper objectMapper = new ObjectMapper();
+                                    String a = "{\n" +
+                                            "    \"success\": false,\n" +
+                                            "    \"message\": \"인증되지 않은 사용자입니다.\",\n" +
+                                            "    \"data\": null,\n" +
+                                            "    \"apiError\": {\n" +
+                                            "        \"msg\": \"인증되지 않은 사용자입니다.\",\n" +
+                                            "        \"status\": 401\n" +
+                                            "    }\n" +
+                                            "}";
+
+                                    response.getWriter().write(a);
+                                })
                 );
         // JwtAuthenticationFilter를 UsernamePasswordAuthenticationFilter 앞에 추가
         // JWT 필터 등록
